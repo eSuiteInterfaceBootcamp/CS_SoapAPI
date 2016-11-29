854a0bdf579c526ea939e6be0eadda2ebee375ee
Cameron Dockstader <cameron.dockstader@gmail.com>
2016-11-29 11:50:59 -0700

    Ready for initial rollout

---
+++ SoapToRest/TicketImport.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eSuiteApiWrapper;
+using SoapToRest.TicketAPI;
+using System.Net;
+using System.IO;
+
+namespace SoapToRest
+{
+    public class TicketImport
+    {
+        private TicketAPIClient _ticketApi;
+        private RestApi _eSuiteApi;
+
+        public TicketImport
+        (
+            string ticketApiURL, string ticketUsername, string ticketPassword,
+            string eSuiteApiURL, string eSuiteApiAuthToken
+        )
+        {
+            _eSuiteApi = new RestApi(eSuiteApiURL, eSuiteApiAuthToken);
+            _ticketApi = new TicketAPIClient();
+            _ticketApi.ClientCredentials.UserName.UserName = ticketUsername;
+            _ticketApi.ClientCredentials.UserName.Password = ticketPassword;
+        }
+
+        public static string GetEcourtUserAuthToken(string username, string password)
+        {
+            return RestApi.GetUserAuthToken(username, password);
+        }
+        public static string CreateUser(string username)
+        {
+            return createUser(username);
+        }
+        public void ImportNewTickets()
+        {
+            Ticket[] tickets;
+
+            try
+            {
+                tickets = _ticketApi.getNewTickets();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Print(ex.Message);
+                System.Diagnostics.Debug.Print(ex.InnerException.Message);
+                throw ex;
+            }
+
+            string personJson, caseJson;
+
+            foreach (Ticket ticket in tickets)
+            {
+                if (getJsonFromTicket(ticket, out personJson, out caseJson))
+                {
+                    int personId;
+                    _eSuiteApi.InsertData("entities/person", personJson, out personId);
+
+                    if (personId > 0)
+                    {
+                        int caseId;
+                        _eSuiteApi.InsertData("entities/case", caseJson, out caseId);
+
+                        if (caseId > 0)
+                        {
+                            int partyId;
+                            _eSuiteApi.InsertData("entities/party", getPartyJson(caseId, personId), out partyId);
+
+                            if (partyId > 0)
+                            {
+                                string chargeJson = getChargeJson(ticket, partyId);
+
+                                if (chargeJson != null)
+                                    _eSuiteApi.InsertData("entities/charge", chargeJson);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private static string createUser(string username)
+        {
+            WebRequest request = WebRequest.Create("https://customdev.journaltech.com/api/rest/signup/" + username);
+
+            if (request is HttpWebRequest == false)
+            {
+                throw new Exception("Incorrect WebRequest type returned.");
+            }
+
+            request.Method = "POST";
+            request.ContentType = "application / json";
+            request.ContentLength = 0;
+            request.Timeout = 10000;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                if (response.StatusCode != HttpStatusCode.Created)
+                {
+                    throw new Exception("API call failed with status code: " + response.StatusCode + ".");
+                }
+
+                using (var stream = response.GetResponseStream())
+                {
+                    if (stream != null)
+                    {
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            string data = reader.ReadToEnd();
+
+                            // parse password
+
+                            return data;
+                        }
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+        }
+        private bool getJsonFromTicket(Ticket ticket, out string personJson, out string caseJson)
+        {
+            personJson = "{ \"firstName\": \"";
+            personJson += ticket.FirstName;
+            personJson += "\", \"middleName\": \"";
+            personJson += ticket.MiddleName;
+            personJson += "\", \"lastName\": \"";
+            personJson += ticket.LastName;
+            personJson += "\", \"identifications\": [ { \"identificationClass\": \"STID\", \"identificationType\": \"DL\", \"identificationNumber\": \"";
+            personJson += ticket.DriversLicenseNumber;
+            personJson += "\" } ] }";
+
+            caseJson = "{ \"caseNumber\": \"";
+            caseJson += ticket.TicketNumber;
+            caseJson += "\" }";
+
+            return true;
+        }
+        private string getPartyJson(int caseId, int personId)
+        {
+            string json = "{ \"case\": { \"id\": ";
+            json += caseId.ToString();
+            json += " }, \"person\": { \"id\": ";
+            json += personId.ToString();
+            json += " } }";
+
+            return json;
+        }
+        private string getChargeJson(Ticket ticket, int partyId)
+        {
+            // get statute id
+            string searchPath = "statute/sectionNumber/" + ticket.StatuteCode;
+            int statuteId;
+            _eSuiteApi.SearchByGet(searchPath, out statuteId);
+
+            if (statuteId < 1)
+            {
+                return null;
+            }
+
+            string json = "{ \"ticketNumber\": \"";
+            json += ticket.TicketNumber;
+            json += "\", \"location\": \"";
+            json += ticket.Location;
+            json += "\", \"associatedParty\": { \"id\": ";
+            json += partyId.ToString();
+            json += " }, \"chargeDate\": \"";
+            json += getJsonDateString(ticket.TicketDate);
+            json += "\", \"statute\": { \"id\": ";
+            json += statuteId.ToString();
+            json += " } }";
+
+            return json;
+        }
+        private string getJsonDateString(DateTime dt)
+        {
+            // example format: 2016-03-03T00:00:00.000-0700",
+            return (dt.ToString("yyyy-MM-ddTHH:mm:ss.fffzzz"));
+        }
+    }
+}
---
+++ SoapToRestConsole/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoapToRest;
+
+namespace SoapToRestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Do you have an existing source account? (Y/n) ");
+            ConsoleKeyInfo existingAccount = Console.ReadKey();
+            Console.WriteLine();
+
+            
+            string src_user = "";
+            string src_pass;
+
+            if (existingAccount.Key == ConsoleKey.Y || existingAccount.Key == ConsoleKey.Enter)
+            {
+                Console.Write("Username: ");
+                src_user = Console.ReadLine();
+
+                Console.Write("Password (not hidden): ");
+                src_pass = Console.ReadLine();
+            }
+            else
+            {
+                Console.Write("Desired username: ");
+                src_user = Console.ReadLine();
+
+                src_pass = TicketImport.CreateUser(src_user);
+                Console.WriteLine(src_pass);
+                src_pass = src_pass.Substring(src_pass.IndexOf(":") + 2);
+            }
+
+            Console.Write("Enter your eCourt URL (e.g. https://server.domain/ecourtpath/): ");
+            string eC_URL = Console.ReadLine();
+
+            Console.Write("Enter your eCourt username: ");
+            string eC_user = Console.ReadLine();
+
+            Console.Write("Enter your eCourt password (not hidden): ");
+            string eC_pass = Console.ReadLine();
+
+
+            TicketImport _ticketImport = new TicketImport(
+                "https://customdev.journaltech.com/api/soap/TicketAPI.svc?wsdl",
+                src_user,
+                src_pass,
+                eC_URL,
+                TicketImport.GetEcourtUserAuthToken(eC_user, eC_pass)
+            );
+
+
+            Console.WriteLine();
+            Console.Write("Ready to roll! Press any key to import all new tickets... ");
+            Console.ReadKey();
+            Console.WriteLine();
+
+            _ticketImport.ImportNewTickets();
+
+            Console.WriteLine("Complete!");
+        }
+    }
+}
